10b94615c525f6b4dd630794b887853cebe2b6cf
Connor's laptop <798759@seq.org>
2018-10-02 18:27:10 -0700

    hi

---
+++ TestMakeup/Problem10.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Problem10 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Problem10
+{
+    // instance variables - replace the example below with your own
+    private int num;
+
+    /**
+     * Constructor for objects of class Problem10
+     */
+    public Problem10()
+    {
+        // initialise instance variables
+         num =0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int pumpkin(int num)
+    {
+        // put your code here
+        get 
+    }
+}
