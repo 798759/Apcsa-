11edb4b96c819fd3aebd46b5962a5817411f0489
cspackman <42615796+cspackman@users.noreply.github.com>
2018-10-05 09:25:02 -0700

    uj

--- Labs/Lab 820/StudentRunner.java
+++ Labs/Lab 820/StudentRunner.java
@@ -1,5 +1,10 @@
 
  public class StudentRunner{
+     
+     
+     
+     
+     
      public static void main(){
             Student S1 =new Student();
             System.out.println(S1.getsnumber());
