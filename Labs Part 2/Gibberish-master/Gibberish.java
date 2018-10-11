e644f2306325596f4f24e0f017825d4f58b2534e
cspackman <42615796+cspackman@users.noreply.github.com>
2018-10-11 10:36:22 -0700

    hi

---
+++ Labs Part 2/Gibberish-master/Gibberish.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Gibberish here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Gibberish
+{
+    private String sourceString; 
+    public void Gibberish(){
+        Template template = new Template();
+
+    }
+
+    public String makeGibberish(WordList5000 wordList5000){
+        int currentSourceIndex = 0; 
+        String resultString = new String(); 
+        while(true){
+            sourceString.indexOf("<");
+            if (sourceString.indexOf("<")!= -1){
+                resultString+= sourceString.substring(currentSourceIndex)
+            }else{
+                
+            }
+            
+            return resultString; 
+
+        }
+ 
+        
+    }
+}
---
+++ Labs Part 2/Gibberish-master/Runner.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Runner here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Runner
+{
+    public static void run(){
+        WordList5000 wordList5000 = new WordList5000();
+        Gibberish Gibberish = new Gibberish();
+        System.out.println(Gibberish);
+    }
+}
