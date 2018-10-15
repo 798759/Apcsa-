161506699671cc1ad4aaa73e2643c63a01b8ae96
cspackman <42615796+cspackman@users.noreply.github.com>
2018-10-15 10:48:43 -0700

    finished

--- Labs Part 2/Gibberish-master/Gibberish.java
+++ Labs Part 2/Gibberish-master/Gibberish.java
@@ -2,32 +2,49 @@
 /**
  * Write a description of class Gibberish here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Connor Spackman)
+ * @version (10/15/18)
  */
 public class Gibberish
 {
     private String sourceString; 
-    public void Gibberish(){
+    
+    public  Gibberish(){
         Template template = new Template();
+        sourceString=template.getSourceString();
 
     }
 
     public String makeGibberish(WordList5000 wordList5000){
-        int currentSourceIndex = 0; 
-        String resultString = new String(); 
-        while(true){
-            sourceString.indexOf("<");
-            if (sourceString.indexOf("<")!= -1){
-                resultString+= sourceString.substring(currentSourceIndex)
+        int currentSourceIndex = 0; //creates variables
+        int nextBracket = 0;
+        int firstIndex = 0;
+        int otherSymbols = 0;
+        String typeOfSpeech;
+        String resultString = new String(); //creates string for answer
+        while(currentSourceIndex<sourceString.length()){
+             firstIndex =sourceString.indexOf("<",currentSourceIndex);//finds where this symbol is located
+            if (firstIndex== -1){//checks if there is a symbol
+                resultString+= sourceString.substring(currentSourceIndex);//if not adds to result
+                currentSourceIndex = sourceString.length();
             }else{
                 
+                resultString+=sourceString.substring(currentSourceIndex,firstIndex);
+                nextBracket=sourceString.indexOf(">",currentSourceIndex+1);//finds next bracket
+                typeOfSpeech=sourceString.substring(firstIndex+1,nextBracket);//gets whats in between bracket
+                if(wordList5000.getRandomWordString(typeOfSpeech)!=null){
+                    resultString+=wordList5000.getRandomWordString(typeOfSpeech);//returns new word
+                }
+                otherSymbols = sourceString.indexOf("</>",nextBracket);//finds close brackets
+                
+                
+                currentSourceIndex = otherSymbols+3; //starts searching after brackets
             }
             
-            return resultString; 
+             
 
         }
  
-        
+        return resultString; // returns answer
     }
 }
--- Labs Part 2/Gibberish-master/Runner.java
+++ Labs Part 2/Gibberish-master/Runner.java
@@ -2,14 +2,17 @@
 /**
  * Write a description of class Runner here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Connor Spackman)
+ * @version (10/15/18)
  */
 public class Runner
 {
     public static void run(){
-        WordList5000 wordList5000 = new WordList5000();
-        Gibberish Gibberish = new Gibberish();
-        System.out.println(Gibberish);
+        WordList5000 wordList5000 = new WordList5000();//creates method to use
+        Gibberish gibberish = new Gibberish();
+
+        String answer;
+        answer = gibberish.makeGibberish(wordList5000);
+        System.out.println(answer);//prints answer
     }
 }
